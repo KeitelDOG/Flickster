d4379a0376233dfb78809069522dcb0cd2f41b91
KeitelDOG <keiteldog@gmail.com>
2016-07-17 16:46:47 -0400

    add special movie item in ListViews for movie voted more than 5 stars

--- app/src/main/java/com/megalobiz/flickster/adapters/MovieArrayAdapter.java
+++ app/src/main/java/com/megalobiz/flickster/adapters/MovieArrayAdapter.java
@@ -21,7 +21,7 @@ import java.util.List;
 public class MovieArrayAdapter extends ArrayAdapter<Movie> {
 
     //View Lookup Cache
-    public static class ViewHolder{
+    public static class ViewHolder {
         TextView title;
         TextView overview;
         ImageView movieImage;
@@ -31,10 +31,28 @@ public class MovieArrayAdapter extends ArrayAdapter<Movie> {
         super(context, android.R.layout.simple_list_item_1, movies);
     }
 
+    //Return int for the type according to popularity of movie (1 or 0)
+    @Override
+    public int getItemViewType(int position) {
+        //return 1 if movie is popular, or 0 if not
+        return getItem(position).getVoteAverage() > 5 ? 1 : 0;
+    }
+
+    //Total number of type is 2 (popular an non-popular)
+    @Override
+    public int getViewTypeCount() {
+        //For Movie popularity, either a Movie is popular, or it is not (2 possibilities)
+        return 2;
+    }
+
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+
         // get the data item for position
         Movie movie = getItem(position);
+
+        //Get data item movie type for this position
+        int type = getItemViewType(position);
 
         // check if an existing view being reused, if not inflate the view
 
@@ -44,7 +62,12 @@ public class MovieArrayAdapter extends ArrayAdapter<Movie> {
         if (convertView == null) {
             viewHolder = new ViewHolder();
             LayoutInflater inflater = LayoutInflater.from(getContext());
-            convertView = inflater.inflate(R.layout.item_movie, parent, false);
+
+            //Inflate the XML layout based on the type
+            convertView = getInflatedLayoutForType(type);
+
+            //Just in case convertView return null, inflate the default item_movie
+            //if(convertView == null) convertView = inflater.inflate(R.layout.item_movie, parent, false);
 
             // find the image view
             viewHolder.movieImage = (ImageView) convertView.findViewById(R.id.ivMovieImage);
@@ -63,18 +86,32 @@ public class MovieArrayAdapter extends ArrayAdapter<Movie> {
         }
 
         // poulate data if viewHolder is not null
-        viewHolder.title.setText(movie.getOriginalTitle());
-        viewHolder.overview.setText(movie.getOverview());
+        // Because item movie popular type contain only movieImage,
+        // title and overview can e null sometimes
+        // Set Text if title and overview are not null
+        if(viewHolder.title != null)
+            viewHolder.title.setText(movie.getOriginalTitle());
+        if(viewHolder.overview != null)
+            viewHolder.overview.setText(movie.getOverview());
 
         //Detect Layout Orientation to change dynamically from posterPath in Portait to backdropPath in Landscape
         int orientation = getContext().getResources().getConfiguration().orientation;
 
-        if(orientation == Configuration.ORIENTATION_PORTRAIT){
-            Picasso.with(getContext()).load(movie.getPosterPath())
-                    .placeholder(R.drawable.movie_placeholder)
-                    .into(viewHolder.movieImage);
-        }
-        else if(orientation == Configuration.ORIENTATION_LANDSCAPE){
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+
+            //load image according to movie item time
+            if(type == 1) {
+                Picasso.with(getContext()).load(movie.getBackdropPath())
+                        .placeholder(R.drawable.movie_placeholder)
+                        .into(viewHolder.movieImage);
+            } else {
+                Picasso.with(getContext()).load(movie.getPosterPath())
+                        .placeholder(R.drawable.movie_placeholder)
+                        .into(viewHolder.movieImage);
+            }
+
+
+        } else if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
             Picasso.with(getContext()).load(movie.getBackdropPath())
                     .placeholder(R.drawable.movie_placeholder)
                     .into(viewHolder.movieImage);
@@ -83,4 +120,18 @@ public class MovieArrayAdapter extends ArrayAdapter<Movie> {
         // return the view
         return convertView;
     }
+
+    //Method to return the correct inflated XML Layout file depending on the item movie popularity
+    private View getInflatedLayoutForType(int type) {
+
+        //if movie is popular inflate item_movie_popular, else inflate item_movie
+        if (type == 1) {
+            return LayoutInflater.from(getContext()).inflate(R.layout.item_movie_popular, null);
+        } else if (type == 0) {
+            return LayoutInflater.from(getContext()).inflate(R.layout.item_movie, null);
+        } else {
+            return null;
+        }
+    }
+
 }
